e4e124411833f8fc7e728bee29142215f8f8d041
Riyakenjaku1 <riyasingh870739@gmail.com>
2024-03-19 12:40:41 +0530

    First Commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+'''print("hello world")
+
+for i in range(5): 
+    print("hello dear")'''
+
+input("Enter your name:")
+x=int(input("Enter the number whoes table is to be printed:"))
+for i in range(1,11):
+    print(x ,"*", i ,"=",x*i)
